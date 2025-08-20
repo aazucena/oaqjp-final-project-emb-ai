5af5146d0d75a923da803de7a6cc265ef31fc3c1
aazucena <aldrinp.azucena@gmail.com>
2025-08-20 09:27:06 -0600

    add code for the emotion detector api

---
+++ emotion_detection.py
@@ -0,0 +1,28 @@
+from requests import post
+import operator
+import json
+
+def emotion_detector(text_to_analyse):
+  print(f'Analyzing text: "{text_to_analyse}"')
+  url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+  json = { "raw_document": { "text": text_to_analyse } }
+  headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+  response = post(url, json=json, headers=headers)
+  formatted_response = json.loads(response.text)
+
+  print(formatted_response)
+  emotion = formatted_response[0]['emotion']
+  anger_score = emotion['anger'] or 0
+  disgust_score = emotion['disgust'] or 0
+  fear_score = emotion['fear'] or 0
+  joy_score = emotion['joy'] or 0
+  sadness_score = emotion['sadness'] or 0
+  dominant_emotion = max(emotion.items(), key=operator.itemgetter(1))[0] or 'unknown'
+  return {
+    'anger': anger_score,
+    'disgust': disgust_score,
+    'fear': fear_score,
+    'joy': joy_score,
+    'sadness': sadness_score,
+    'dominant_emotion': dominant_emotion
+  }
